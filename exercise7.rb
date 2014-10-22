5be60dcdb92ed5b4073b05a102419804c901c109
NATHAN <nathan.ho28@gmail.com>
2014-10-22 17:34:54 -0400

    finished question 4 for Exercise 7.

--- exercise7.rb
+++ exercise7.rb
@@ -10,13 +10,13 @@ puts "How many students have there been per cohort?"
 
 def cohorts(n)
 	n.each do |co, stu|
-	puts "#{co}: #{stu} students"
+		puts "#{co}: #{stu} students"
 	end
 end
 
 cohorts(students)
 
-#Exercise 7: Question 2
+#Exercise 7: Question 2 
 
 puts "Add the fourth cohort to the list."
 puts "Added! (See below)" 
@@ -27,7 +27,24 @@ cohorts(students)
 
 #Exercise 7: Question 3
 
+puts "What are all the keys in my hash?"
+
 puts students.keys
+
+#Exercise 7: Question 4
+
+puts "Wow Bitmaker is popular! All classes have increased by 5%"
+
+students.each do |co, stu| 
+	students[co] = stu * 1.05
+	# puts "#{co}: #{stu} students"
+end
+
+cohorts(students)
+
+
+
+
 
 
 
